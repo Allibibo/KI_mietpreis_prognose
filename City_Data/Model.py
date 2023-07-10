a89c4cdafa756a2f338f55051813eb02e9e2b764
alexander <alexander.hofhammer@googlemail.com>
2023-07-10 14:08:53 +0200

    Alex

--- City_Data/Model.py
+++ City_Data/Model.py
@@ -1,22 +1,21 @@
 import json
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
+from tensorflow.keras import regularizers
 
-if __name__ == '__main__':
 
-    with open('training_data.json', 'r') as r:
-        full_data = json.load(r)
-
+def _train_deep_neural_network(model_name, full_data):
     features = []
     labels = []
     for data in full_data:
-        # f = [data['roomCount'], data['propertyAge']]
         f = [data['roomCount'], data['propertyAge'], data['livingSpace'], data['hasBasement'], data['hasBalcony'],
-             data['parkingLotCount'], data['hasGarden'], data['hasElevator'], data['year'] * 100, data['houseType apartment'],
+             data['parkingLotCount'], data['hasGarden'], data['hasElevator'], data['year'] * 100,
+             data['houseType apartment'],
              data['houseType ground_floor'], data['houseType half_basement'], data['houseType roof_storey'],
              data['houseType maisonette'], data['houseType raised_ground_floor'], data['houseType terraced_flat'],
              data['houseType other'], data['houseType penthouse'], data['houseType loft'], data['bundesland Berlin'],
@@ -42,17 +41,48 @@ if __name__ == '__main__':
     X_test = scaler.transform(X_test)
 
     # Definition der Hyperparameter
+    input_param_count = len(full_data[0]) -1
+
+    # Erstellung des Modells
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(512, activation='relu', input_shape=(input_param_count,), kernel_regularizer=regularizers.l2(0.001)),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1)
+    ])
+
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='mse', metrics=['mae'])
+
+    model.fit(X_train, y_train, epochs=20, batch_size=512, validation_data=(X_test, y_test), callbacks=[tf.keras.callbacks.EarlyStopping(patience=5)])
+
+    model.summary()
+
+    test_loss, test_mae = model.evaluate(X_test, y_test)
+    print('Test Loss:', test_loss)
+    print('Test MAE:', test_mae)
+
+    model.save(model_name)
+
+
+def _train_LSTM_model(model_name, full_data):
+
+    features, labels = prepare_data_for_lstm(full_data)
+
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+
+    # Definition der Hyperparameter
     num_units = 64  # Anzahl der Einheiten (Neuronen) pro Schicht
     num_layers = 2  # Anzahl der RNN-Schichten
     input_shape = X_train.shape[1:]  # Eingabeform (Shape)
 
     # Erstellung des Modells
     model = tf.keras.Sequential()
-
-    # Hinzufügen der RNN-Schichten
-    for _ in range(num_layers):
-        model.add(tf.keras.layers.LSTM(32, input_shape=(None, len(full_data[0]) -1)))
-        model.add(tf.keras.layers.Dense(1))  # Dropout zur Regularisierung
+    model.add(tf.keras.layers.LSTM(64, input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(tf.keras.layers.Dense(1))  # Dropout zur Regularisierung
 
     # Ausgabeschicht
     model.add(tf.keras.layers.Dense(units=1))
@@ -71,4 +101,71 @@ if __name__ == '__main__':
 
     print(model.summary())
 
-    model.save("Model")
+    model.save(model_name)
+
+
+def prepare_data_for_lstm(data):
+    X = []
+    y = []
+
+    for time_steps in data:
+        X_timestep = []
+        y_timestep = []
+
+        for timestep_data in time_steps:
+            X_timestep.append([
+                timestep_data['roomCount'],
+                timestep_data['propertyAge'],
+                timestep_data['livingSpace'],
+                timestep_data['hasBasement'],
+                timestep_data['hasBalcony'],
+                timestep_data['parkingLotCount'],
+                timestep_data['hasGarden'],
+                timestep_data['hasElevator'],
+                timestep_data['year'],
+                timestep_data['houseType apartment'],
+                timestep_data['houseType ground_floor'],
+                timestep_data['houseType half_basement'],
+                timestep_data['houseType roof_storey'],
+                timestep_data['houseType maisonette'],
+                timestep_data['houseType raised_ground_floor'],
+                timestep_data['houseType terraced_flat'],
+                timestep_data['houseType other'],
+                timestep_data['houseType penthouse'],
+                timestep_data['houseType loft'],
+                timestep_data['bundesland Berlin'],
+                timestep_data['bundesland Bremen'],
+                timestep_data['bundesland Nordrhein Westfalen'],
+                timestep_data['bundesland Hamburg'],
+                timestep_data['bundesland Sachsen Anhalt'],
+                timestep_data['bundesland Niedersachsen'],
+                timestep_data['bundesland Baden Wuerttemberg'],
+                timestep_data['bundesland Rheinland Pfalz'],
+                timestep_data['bundesland Hessen'],
+                timestep_data['bundesland Brandenburg'],
+                timestep_data['bundesland Sachsen'],
+                timestep_data['bundesland Thueringen'],
+                timestep_data['bundesland Bayern'],
+                timestep_data['bundesland Mecklenburg Vorpommern'],
+                timestep_data['bundesland Schleswig Holstein'],
+                timestep_data['bundesland Saarland'],
+                timestep_data['city_avr_rating'],
+                timestep_data['city_avr_acc_population_change'],
+                timestep_data['city_avr_population_change_last_year'],
+                timestep_data['city_avr_persons_per_km2'],
+                timestep_data['closest_city_distance']
+            ])
+            y_timestep.append(timestep_data['rent'])
+
+        X.append(X_timestep)
+        y.append(y_timestep)
+
+    return np.array(X), np.array(y)
+
+if __name__ == '__main__':
+
+    with open('test_data_2.json', 'r') as r:
+        full_data = json.load(r)
+    _train_deep_neural_network("Model/m1", full_data)
+
+    #_train_LSTM_model("Model/m2", full_data)
--- City_Data/main.py
+++ City_Data/main.py
@@ -1,4 +1,6 @@
 # Main Klasse für das Starten des Projekts
+import json
+
 from City_Data import city_data_service
 
 if __name__ == '__main__':
@@ -8,5 +10,15 @@ if __name__ == '__main__':
     #for city_data in temp:
     #    print(city_data_service.get_average_rating(city_data, 2019))
     #city_data_service.test()
-    temp = city_data_service.get_location_statistics(52.1085288, 11.619955, 2013)
-    print(temp)
+    #temp = city_data_service.get_location_statistics(52.1085288, 11.619955, 2013)
+    #print(temp)
+
+# Hinzufügen von populations daten
+#     file_name = 'city_population_data.json'
+#     with open(file_name, 'r', encoding='utf-8') as file:
+#         loaded_data = json.load(file)
+#
+#     data = city_data_service.fill_up_to_2025(loaded_data)
+#
+#     with open('city_population_data.json', 'w', encoding='utf-8') as file:
+#         json.dump(data, file, ensure_ascii=False, indent=2)
